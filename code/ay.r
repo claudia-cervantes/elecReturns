e592d4fc5c5fbc45c7b15447b2882ceacff579b9
Eric Magar <emagar@gmail.com>
2017-10-20 16:11:15 -0500

    Added more elections in 2012 to csv

--- code/ay.r
+++ code/ay.r
@@ -5,12 +5,32 @@ dat <- read.csv(file = "aymu1977-present.csv", stringsAsFactors = FALSE)
 
 colnames(dat)
 
-sel <- which(dat$pan>0 & dat$l01 == "pan")
-length (sel)
-dat.tmp <- dat[sel,] # subset
-sel2 <- which(dat.tmp$pan==dat.tmp$v01)
-length (sel2)
-dat.tmp$pan[sel2] <- 0
-dat[sel,] <- dat.tmp
+# re-compute effective and total votes
+sel <- grep("^v[0-9]{2}", colnames(dat))
+v <- dat[,sel] # subset votes columns
+head(v)
+colSums(v)
+v$efec <- round(rowSums(v), 0)
+dat$efec <- v$efec
+v$tot <- dat$efec + dat$nr + dat$nulos
+dat$tot <- v$tot
+rm(v)
+
+## # datos faltantes y si tengo votos brutos
+## col2012 no
+## cua2013 sí
+## dgo2013 sí pdf seccion
+## oax2013 no
+## pue2013 sí
+## que2012 no
+## qui2013 no
+## san2012 no
+## sin2013 no
+## son2012 no
+## tab2012 no
+## tam2013 no
+## tla2013 no
+## ver2013 no
+## zac2013 no
 
 write.csv(dat, file = "aymu1977-present.csv", row.names = FALSE)
