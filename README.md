43eaba793e401ac261e0ff7ab177cee00575bb2c
Eric Magar <emagar@gmail.com>
2017-10-23 15:45:00 -0500

    In ay.r starts coalition identification

--- README.md
+++ README.md
@@ -1,15 +1,52 @@
-**Recent Mexican election vote returns repository**
+<div id="table-of-contents">
+<h2>Table of Contents</h2>
+<div id="text-table-of-contents">
+<ul>
+<li><a href="#orgc141322">1. Recent Mexican election vote returns repository</a>
+<ul>
+<li><a href="#org60060b7">1.1. Files in the reporitory</a></li>
+<li><a href="#org90e4cc6">1.2. Sources</a></li>
+<li><a href="#org65ad266">1.3. Codebook</a></li>
+</ul>
+</li>
+</ul>
+</div>
+</div>
+
+<a id="orgc141322"></a>
+
+# Recent Mexican election vote returns repository
+
 by **Eric Magar**
+
 email: emagar at itam dot mx
+
 *work in progress Oct-22-2017*
----
 
-# Files in the reporitory
+&#x2014;
 
-In general, file names include the office elected (df, dl, pr, ay, etc. for diputados federales, diputados locales, presidente, ayuntamiento, etc. respectively), followed by the unit of observation (df, mu, se, ca for distrito federal, municipio, seccion, casilla respectively), followed by the years included. 
 
-* aymu1977-on.csv
-* dfdf1979-on.csv
-* prdf2006-on.csv
+<a id="org60060b7"></a>
 
-# Sources
+## Files in the reporitory
+
+In general, file names identify the office elected (e.g., df, dl, pr, ay, etc. for diputados federales, diputados locales, presidente, ayuntamiento, etc. respectively), followed by the unit of observation (df, mu, se, ca for distrito federal, municipio, seccion, casilla respectively), followed by the years included. 
+
+-   aymu1977-on.csv
+-   dfdf1979-on.csv
+-   prdf2006-on.csv
+
+
+<a id="org90e4cc6"></a>
+
+## Sources
+
+
+<a id="org65ad266"></a>
+
+## Codebook
+
+-   edon
+-   disn
+-   munn
+
--- code/ay.r
+++ code/ay.r
@@ -1,6 +1,9 @@
+rm(list = ls())
+
 dd <- "/home/eric/Desktop/MXelsCalendGovt/elecReturns/"
 setwd(dd)
 
+# read raw data file
 dat <- read.csv(file = "aymu1977-present.csv", stringsAsFactors = FALSE)
 
 colnames(dat)
@@ -16,12 +19,12 @@ v$tot <- dat$efec + dat$nr + dat$nulos
 dat$tot <- v$tot
 rm(v)
 
-## # datos faltantes y si tengo votos brutos
-## col2012 no
+## # julio: lista de datos faltantes y si tengo votos brutos que falta sistematizar
 ## cua2013 sí, por seccion
-## dgo2013 sí, por seccion en pdf
-## oax2013 no
 ## pue2013 sí, por seccion
+## col2012 no
+## dgo2013 no
+## oax2013 no
 ## que2012 no
 ## qui2013 no
 ## san2012 no
@@ -32,5 +35,40 @@ rm(v)
 ## tla2013 no
 ## ver2013 no
 ## zac2013 no
+
+
+#####################################################
+#####################################################
+# identify coalition by searching for "-" in labels #
+#####################################################
+#####################################################
+sel <- grep("^l[0-9]{2}", colnames(dat))
+l <- dat[,sel] # subset label columns
+sel <- grep("^v[0-9]{2}", colnames(dat))
+v <- dat[,sel] # subset votes columns
+
+ln1 <- v # duplicate votes
+ln1[] <- 0 # will receive lengths
+ln2 <- ln1 # duplicate
+# replace each label with its character length
+for (i in 1:ncol(ln1)){
+    #i <- 18 # debug
+    tmp <- l[,i] # pick ith column as vector
+    ln1[,i] <- nchar(tmp) # replace items with num characters
+}
+
+# drop hyphens to count character difference
+l.tmp <- l # duplicate
+for (i in 1:ncol(l.tmp)){
+    #i <- 1 # debug
+    l.tmp[,i] <- gsub("-", "", l.tmp[,i]) # remove hyphens to count char dif
+    ln2[,i] <- nchar(l.tmp[,i]) # replace items with num characters
+}
+
+npInCoal <- ln1 - ln2 + 1 # 1 means no coalition, single party
+rm(i, ln1, ln2, l.tmp, sel, tmp)
+ls()
+
+
 
 write.csv(dat, file = "aymu1977-present.csv", row.names = FALSE)
